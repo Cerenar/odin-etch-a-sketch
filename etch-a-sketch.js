234862d66f89e520b8c1eb50632719b5c116e0a2
Cameron <camerondonner63@gmail.com>
2021-10-25 01:09:23 -0500

    Got color/gray drawing, grid clear working.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,23 @@
 const gameboy = document.querySelector("#gameboy");
 const container = document.querySelector("#container");
+const recolor = document.querySelector(".recolor");
+const clear = document.querySelector(".clear");
+const resize = document.querySelector(".resize");
+const grayscale = document.querySelector(".grayscale");
+const color = document.querySelector(".color");
 
 function paintField () {
 	const randomColor = Math.floor(Math.random()*16777215).toString(16);
-	this.style.backgroundColor = `#${randomColor}`;
+	gameboy.style.backgroundColor = `#${randomColor}`;
 }
 
 function colorMe () {
 	const randomColor = Math.floor(Math.random()*16777215).toString(16);
 	this.style.backgroundColor = `#${randomColor}`;
+}
+
+function colorMeGray () {
+	this.style.backgroundColor = "#000000";
 }
 
 for (let i = 0; i < 16; i++) {
@@ -23,6 +32,22 @@ for (let i = 0; i < 16; i++) {
 	container.appendChild(row);
 }
 
+function makeGrayscale () {
+	boxes.forEach(box => box.addEventListener("mouseenter", colorMeGray));
+	boxes.forEach(box => box.removeEventListener("mouseenter", colorMe));
+}
+
+function makeColor () {
+	boxes.forEach(box => box.addEventListener("mouseenter", colorMe));
+	boxes.forEach(box => box.removeEventListener("mouseenter", colorMeGray));
+}
+
+function clearGrid () {
+	boxes.forEach(box => box.style.backgroundColor = "#999999");
+}
+
 const boxes = document.querySelectorAll(".box");
-boxes.forEach(box => box.addEventListener("mouseenter", colorMe));
-gameboy.addEventListener("mousedown", paintField);
+recolor.addEventListener("click", paintField);
+clear.addEventListener("click", clearGrid);
+grayscale.addEventListener("click", makeGrayscale);
+color.addEventListener("click", makeColor);
