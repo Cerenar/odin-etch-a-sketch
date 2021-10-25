10769234f0f545c0b96b5c544933e0c14ec00204
Cameron <camerondonner63@gmail.com>
2021-10-25 11:25:10 -0500

    Added color picker.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,6 +5,8 @@ const clear = document.querySelector(".clear");
 const resize = document.querySelector(".resize");
 const grayscale = document.querySelector(".grayscale");
 const color = document.querySelector(".color");
+const colorSelect = document.querySelector(".color-select");
+const hexCodeRegex = "#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
 
 function paintField () {
 	const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -18,6 +20,10 @@ function colorMe () {
 
 function colorMeGray () {
 	this.style.backgroundColor = "#000000";
+}
+
+function colorMeChoice (box, colorChoice) {
+	box.style.backgroundColor = colorChoice;
 }
 
 for (let i = 0; i < 16; i++) {
@@ -35,11 +41,31 @@ for (let i = 0; i < 16; i++) {
 function makeGrayscale () {
 	boxes.forEach(box => box.addEventListener("mouseenter", colorMeGray));
 	boxes.forEach(box => box.removeEventListener("mouseenter", colorMe));
+	boxes.forEach(box => box.removeEventListener("mouseenter", colorMeChoice));
 }
 
 function makeColor () {
 	boxes.forEach(box => box.addEventListener("mouseenter", colorMe));
 	boxes.forEach(box => box.removeEventListener("mouseenter", colorMeGray));
+	boxes.forEach(box => box.removeEventListener("mouseenter", colorMeChoice));
+}
+
+function selectColor () {
+	let validation = true;
+	while (validation) {
+		let colorChoice = prompt ("What color would you like to draw in?", "#000000");
+		if (!colorChoice.match(hexCodeRegex)) {
+			alert ("Please use a valid hex color code.");
+		}
+		else {
+			boxes.forEach(box => box.addEventListener("mouseenter", () => {
+				colorMeChoice (box, colorChoice);
+			}));
+			boxes.forEach(box => box.removeEventListener("mouseenter", colorMe));
+			boxes.forEach(box => box.removeEventListener("mouseenter", colorMeGray));
+			validation = false;
+		}
+	}
 }
 
 function clearGrid () {
@@ -51,3 +77,4 @@ recolor.addEventListener("click", paintField);
 clear.addEventListener("click", clearGrid);
 grayscale.addEventListener("click", makeGrayscale);
 color.addEventListener("click", makeColor);
+colorSelect.addEventListener("click", selectColor);
