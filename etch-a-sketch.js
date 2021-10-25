9e4149746764d76a44933482662abedfa71227e5
Cameron <camerondonner63@gmail.com>
2021-10-24 19:54:01 -0500

    Added pretty UI.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,28 @@
+const gameboy = document.querySelector("#gameboy");
 const container = document.querySelector("#container");
 
-function colorMe () {
-	this.classList.add("colorful");
+function paintField () {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	this.style.backgroundColor = `#${randomColor}`;
 }
 
-for (let i = 0; i < 32; i++) {
+function colorMe () {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	this.style.backgroundColor = `#${randomColor}`;
+}
+
+for (let i = 0; i < 16; i++) {
 	const row = document.createElement("div");
 	row.classList.add("row");
-	for (let j = 0; j < 32; j++) {
+	for (let j = 0; j < 16; j++) {
 		const box = document.createElement("div");
 		box.classList.add("box");
 		row.appendChild(box);
-		box.style.height = `${100/32}%`;
+		box.style.height = `${100/16}%`;
 	}
 	container.appendChild(row);
 }
 
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => box.addEventListener("mouseenter", colorMe));
+gameboy.addEventListener("mousedown", paintField);
